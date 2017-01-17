281bc1eef81c0e08c1a80e29dc3e6cfd95c17383
leo <leo.moralesvega@gmail.com>
2017-01-17 15:43:27 +0100

    Implementing missing methods in AdminService and added missing methods in AdminRepository

--- src/main/java/se/plushogskolan/restcaseservice/repository/AdminRepository.java
+++ src/main/java/se/plushogskolan/restcaseservice/repository/AdminRepository.java
@@ -6,4 +6,7 @@ import se.plushogskolan.restcaseservice.model.Admin;
 
 public interface AdminRepository extends PagingAndSortingRepository<Admin, Long> {
 
+	public Admin findByUsername(String username);
+	
+	public Admin findByToken(String token);
 }
--- src/main/java/se/plushogskolan/restcaseservice/service/AdminService.java
+++ src/main/java/se/plushogskolan/restcaseservice/service/AdminService.java
@@ -2,13 +2,14 @@ package se.plushogskolan.restcaseservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import se.plushogskolan.restcaseservice.exception.UnathorizedException;
 import se.plushogskolan.restcaseservice.exception.WebInternalErrorException;
 import se.plushogskolan.restcaseservice.model.Admin;
 import se.plushogskolan.restcaseservice.repository.AdminRepository;
 
-@Component
+@Service
 public class AdminService {
 	
 	private AdminRepository adminRepository;
@@ -21,15 +22,52 @@ public class AdminService {
 	public Admin save(String username, String password) {
 		Admin admin = createAdmin(username, password);
 		try {
-			admin = adminRepository.save(admin);
+			return adminRepository.save(admin);
 		} catch(DataAccessException e) {
 			throw new WebInternalErrorException("Could not save admin");
 		}
-		return admin;
 	}
-
+	
+	public String login(String username, String password) {
+		Admin admin = adminRepository.findByUsername(username);
+		if(authenticateLogin(admin, password)) {
+			String token = newToken();
+			admin.setToken(token);
+			adminRepository.save(admin);
+			return token;
+		}
+		else
+			throw new UnathorizedException("Invalid login");
+	}
+	
+	public boolean authenticateToken(String token) {
+		Admin admin;
+		try {
+			admin = adminRepository.findByToken(token);
+		} catch(DataAccessException e) {
+			throw new WebInternalErrorException("Internal error");
+		}
+		return true;
+	}
+	
+	
 	private Admin createAdmin(String username, String password) {
-		
+		//TODO implement
+		return null;
+	}
+	
+	private boolean authenticateLogin(Admin admin, String password) {
+		//TODO implement
+		return false;
+	}
+	
+	private String newToken() {
+		//TODO secure.random-ly generate Token
+		return null;
+	}
+	
+	private byte[] hash(String string) {
+		//TODO hash
 		return null;
 	}
 }
