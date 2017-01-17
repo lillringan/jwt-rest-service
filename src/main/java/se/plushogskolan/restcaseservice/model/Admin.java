96f241db050e94af6db9ae906dd1067a849b5e5c
Joakim Landström <joakim.landstrom@outlook.com>
2017-01-17 15:02:28 +0100

    admin changes

--- src/main/java/se/plushogskolan/restcaseservice/model/Admin.java
+++ src/main/java/se/plushogskolan/restcaseservice/model/Admin.java
@@ -2,6 +2,7 @@ package se.plushogskolan.restcaseservice.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import se.plushogskolan.casemanagement.model.AbstractEntity;
@@ -9,14 +10,17 @@ import se.plushogskolan.casemanagement.model.AbstractEntity;
 @Entity
 public class Admin extends AbstractEntity {
 
+	@Column(unique=true)
 	private String username;
 	
+	@Column(unique=true)
 	private String salt;
 	
 	private String hashedPassword;
 	
 	private LocalDateTime timestamp;
 	
+	@Column(unique=true)
 	private String token;
 	
 	protected Admin(){
@@ -46,5 +50,13 @@ public class Admin extends AbstractEntity {
 	
 	public String getToken() {
 		return token;
+	}
+	
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
 	}
 }
