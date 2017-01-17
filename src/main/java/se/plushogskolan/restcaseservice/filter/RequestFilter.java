fedf2924489db7e23ecc1b191474fe76974d7dad
lillringan <lillringan@gmail.com>
2017-01-17 16:02:57 +0100

    Changed requestfilter so it works

--- src/main/java/se/plushogskolan/restcaseservice/filter/RequestFilter.java
+++ src/main/java/se/plushogskolan/restcaseservice/filter/RequestFilter.java
@@ -6,21 +6,29 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import se.plushogskolan.restcaseservice.exception.UnathorizedException;
+import se.plushogskolan.restcaseservice.service.AdminService;
 
 @Provider
 public final class RequestFilter implements ContainerRequestFilter {
 
 	public static final String AUTH_TOKEN = "auth";
+	
+	@Autowired
+	AdminService adminService;
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 
 		String authHeader = requestContext.getHeaderString("Authorization");
 		
-		System.out.println(requestContext.getUriInfo().getRequestUri());
+		System.out.println(requestContext.getUriInfo().getRequestUri().getRawPath());
 		
-		if (authHeader == null || !authHeader.equalsIgnoreCase(AUTH_TOKEN)) {
+		if("/login".equals(requestContext.getUriInfo().getRequestUri().getRawPath())){} 
+		else if(adminService.authenticateToken(requestContext.getHeaderString("Authorization"))){}
+		else if (authHeader == null || !authHeader.equalsIgnoreCase(AUTH_TOKEN)) {
 			throw new UnathorizedException("Unathorized");
 		}
 		
